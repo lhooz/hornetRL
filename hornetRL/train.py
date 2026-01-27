8a4fad94cf42b50506949416bd995a1a660f8a87
Hao Li <haoli@Haos-MacBook-Pro-2.local>
2026-01-27 23:12:06 +0800

    updated symlog obs scale, and pos reward

--- hornetRL/train.py
+++ hornetRL/train.py
@@ -74,7 +74,7 @@ class Config:
         2.0,     # Th_Ang (Orientation penalty)
         1.0,     # Ab_Ang (Abdomen stability)
         0.1,     # Lin_Vel (Drift damping)
-        0.001,    # Ang_Vel (Vibration damping)
+        0.0,     # Ang_Vel (Vibration damping)
         0.1      # Eff (Force efficiency)
     ])
     
