9ca106f04b0d289861cc7934e6dd2911e74b9fc7
Hao Li <haoli@Haos-MacBook-Pro-2.local>
2026-01-30 13:13:05 +0800

    add action randomlisation for tanh

--- hornetRL/train.py
+++ hornetRL/train.py
@@ -90,7 +90,7 @@ class Config:
 
     CKPT_DIR = "checkpoints_shac"
     VIS_DIR = "checkpoints_shac"
-    AUX_LOSS_WEIGHT = 0.1 
+    AUX_LOSS_WEIGHT = 0.01 
     VIS_INTERVAL = 200      
     
     WARMUP_STEPS = 1        # Control steps to pin the fly before releasing dynamics.
